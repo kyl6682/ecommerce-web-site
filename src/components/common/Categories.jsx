bbbc60f2c7b25d5710fe6b44118bfdf2d4775615
yurim <kyl6682@gmail.com>
2025-03-19 16:46:02 +0900

    🍱 Categories 데이터 컴포넌트 및 아이콘 생성

---
+++ src/components/common/Categories.jsx
@@ -0,0 +1,35 @@
+import Electronics from "../../assets/icons/Electronics"
+import Furniture from "../../assets/icons/Furniture"
+import Other from "../../assets/icons/Other"
+import Shoes from "../../assets/icons/Shoes"
+import TShirt from "../../assets/icons/T-Shirt"
+
+
+export const categories = [ 
+    {
+        id : 1,
+        name : "T-shirt",
+        icon : TShirt,
+        link : "#"
+    }, {
+        id : 2,
+        name : "Electronics",
+        icon : Electronics,
+        link : "#"
+    }, {
+        id : 3,
+        name : "Furniture",
+        icon : Furniture,
+        link : "#"
+    }, {
+        id : 4,
+        name : "Shoes",
+        icon : Shoes,
+        link : "#"
+    }, {
+        id : 5,
+        name : "Other",
+        icon : Other,
+        link : "#"
+    },
+]
--- src/components/home/Categories.jsx
+++ src/components/home/Categories.jsx
@@ -1,7 +1,28 @@
-function Categories () {
-    return (
-        <div>Categories</div>
-    )
+import styled from 'styled-components'
+import { Wrapper } from '../../styles/CommonStyle'
+import { categories } from '../common/categories'
+
+const CategoryCard = styled(Wrapper)`
+  width: 160px;
+  height: 120px;
+  background-color: #ededed;
+  border-radius: 15px;
+`
+
+function Categories() {
+  return (
+    <Wrapper>
+      {categories.map((el) => {
+        const IconComponent = el.icon
+        return (
+          <CategoryCard key={el.id} href={el.link}>
+            {IconComponent ? <IconComponent /> : '준비중'}
+            <span>{el.name}</span>
+          </CategoryCard>
+        )
+      })}
+    </Wrapper>
+  )
 }
 
 export default Categories
